b21dd1008b54e92b4564c3f1aeca449307c788da
Takashi Kanemoto <ttskch@gmail.com>
2022-10-02 00:51:37 +0900

    feat: 🎸 add github link

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,11 +1,19 @@
-import {Box, Flex} from '@chakra-ui/react'
+import {Box, Flex, Icon} from '@chakra-ui/react'
+import {AiFillGithub} from 'react-icons/ai'
 import {NarrowBox} from '../molecules/NarrowBox'
 
 export const Footer: React.FC = () => {
   return (
     <Box as="footer" bg="gray.700" color="white">
       <NarrowBox py="1.5rem" fontSize="sm">
-        <Flex>
+        <Flex align="center">
+          <a
+            href="https://github.com/ttskch/lemira"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Icon as={AiFillGithub} fontSize="1.5rem" />
+          </a>
           <Flex align="center" gap="0.2em" ml="auto">
             <a
               href="https://twitter.com/ttskch"
